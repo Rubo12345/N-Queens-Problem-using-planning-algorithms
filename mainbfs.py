2eaf33e3d4dc0d8f7fd622b2ead65f61dda1ae46
Rutwik <rrbonde@wpi.edu>
2022-09-15 09:16:16 -0400

    Added the Astar function to the code

--- mainbfs.py
+++ mainbfs.py
@@ -1,14 +1,14 @@
 from queue import Empty
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-import csv
+import math
 import numpy as np
 import random
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.font_manager import FontProperties
 from collections import deque
-from attacking_queens import *
+from Attacking_Queens import *
 
 class Queen:
     def __init__(self):
@@ -16,6 +16,7 @@ class Queen:
         self.col = 0                    #col coordinate of queen
         self.board_size = 0             #board size
         self.positions = []             #initial positions of queens in the board                    
+        self.weights = []
 
     def setcoords(self, m):
         self.row = m[0]
@@ -29,6 +30,7 @@ class Queen:
             for y in range(self.board_size):
                 if current_board[x][y] != 0:
                     self.positions.append((x,y))
+                    self.weights.append((current_board[x][y]))
 
     def is_valid(self, position):
         '''Check if the new position is within the chessboard'''
@@ -51,7 +53,6 @@ class Queen:
             return new_board
 
 def plot(board, pos_queens, title):
-
     #Show chess board
     chessboard = np.array([[(i+j)%2 for i in range(len(board))] for j in range(len(board))])
     plt.imshow(chessboard,cmap='ocean')
@@ -76,7 +77,6 @@ def takeSecond(elem):
     return elem[1]
 
 def bfs(init_board_state, board_size):
-
     flag = 0
     queens = Queen()
     #Initialize queue and visited board configuration
@@ -106,7 +106,6 @@ def bfs(init_board_state, board_size):
                 for j in range(-board_size+1, board_size):
                     queens.setcoords(queen)
                     new_state = queens.movequeen(j, current_state)
-                    
                     is_in_visited = any(np.array_equal(new_state, x) for x in visited)
                     if not is_in_visited:
                         if new_state is not None:
@@ -119,15 +118,95 @@ def bfs(init_board_state, board_size):
     else:
         print("No solution was found.")
 
+def g_cost(current_state, previous_state):
+    g = 0
+    queens = Queen()
+    if previous_state == None:
+        return 0
+    else:
+        queens.getpositions(current_state)
+        cs = queens.positions
+        queens.positions = []
+        queens.getpositions(previous_state)
+        ps = queens.positions
+        for i in range(len(cs)):
+            if cs[i] != ps[i]:
+                g += math.sqrt((cs[i][0] - ps[i][0])**2 + (cs[i][1] - ps[i][1])**2)
+    return g
+
+def Astar(init_board_state, board_size):
+    flag = 0
+    new_state_g=0
+    queens = Queen()
+    Open_List = []
+    Open_List_States = []
+    Closed_List = []
+    Open_List.append((init_board_state,np.inf,0,np.inf))
+    Open_List_States.append((init_board_state))
+
+    while Open_List:
+        Open_List.sort(key = takeSecond)
+        m = Open_List.pop(0)
+        current_state = m[0]
+        f_cost = m[1]
+        g_cost = m[2]
+        h_cost = m[3]
+
+        queens.getpositions(current_state)
+        # print(h_cost)
+        if h_cost == 0:
+            flag = 1
+            break
+
+        for i in range(len(queens.positions)):
+            for queen in queens.positions:
+                cost = g_cost + queens.weights[queens.positions.index(queen)]**2
+                for j in range(-board_size+1, board_size):
+                    queens.setcoords(queen)
+                    queen_weight = queens.weights[queens.positions.index(queen)]
+                    new_state = queens.movequeen(j, current_state)
+                    new_state_g += queen_weight**2
+
+                    # new_state_h = attackingpairs(new_state)
+                    # new_state_cost = new_state_g + new_state_h
+                    # a = (new_state,new_state_cost,new_state_g,new_state_h)
+
+                    # for i in range(len(Open_List)):
+                    #     if new_state == Open_List[i][0]:
+                    #         if new_state_g <= cost:
+                    #             continue
+
+                    # for j in range(len(Closed_List)):
+                    #     if new_state == Closed_List[i][0]:
+                    #         if new_state_g <= cost:
+                    #             continue
+
+                    if (new_state,new_state_cost,new_state_g,new_state_h) in Open_List: #This is wrong
+                        if new_state_g <= cost:
+                            continue
+                    if (new_state,new_state_cost,new_state_g,new_state_h) in Closed_List:
+                        if new_state_g <= cost:
+                            continue
+                    else:
+                        new_state_h = attackingpairs(new_state)
+                        new_state_cost = new_state_g + new_state_h
+                        Open_List.append((new_state,new_state_cost,new_state_g,new_state_h))
+                    new_state_g = cost
+                    new_state_cost = new_state_h + new_state_g
+
+        Closed_List.append(current_state)
+    return current_state, queens.positions
+
 if __name__ == "__main__":
 
     board_size = 5
     weight_range = 8
 
-    #Generate the initial random configuration of the board
+    # Generate the initial random configuration of the board
     init_board_state, init_pos = generate_configuration(board_size, weight_range)
-
-    solution, queens_pos = bfs(init_board_state, board_size)
+    # print(init_board_state)
+    # solution, queens_pos = bfs(init_board_state, board_size)
+    solution, queens_pos = Astar(init_board_state,board_size )
     plt.figure(1)
     plot(init_board_state, init_pos, 'Initial Configuration')
     plt.figure(2)
